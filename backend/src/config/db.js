4dcefad33fd8063368622cbbea9d9f64c3bcc6ea
Sparsh Balodia <sparshbalodia2003@gmail.com>
2026-05-27 16:06:44 +0530

    fix: railway db fixed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,15 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const { Pool } = pg;
 
 const pool = new Pool({
-  host:     process.env.DB_HOST,
-  port:     process.env.DB_PORT,
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect()
