834ea44aaf124aaaca1ff43d4223c144891ae359
Sparsh Balodia <sparshbalodia2003@gmail.com>
2026-05-27 15:33:27 +0530

    fix : deployed on railway

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import app from './src/app.js';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 import customerRoutes from './routes/customerRoutes.js';
@@ -10,13 +11,27 @@ import errorHandler from './middleware/errorHandler.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5174',
+    process.env.FRONTEND_URL,
+  ],
+  credentials: true,
+}));
+
 app.use(express.json());
 
+/*Root Route*/
+app.get("/", (req, res) => {
+  res.send("Billing Dashboard Backend Running");
+});
+
+/*API Routes*/
 app.use('/api/customers', customerRoutes);
 app.use('/api/items', itemRoutes);
 app.use('/api/invoices', invoiceRoutes);
 
+/*Error Handler*/
 app.use(errorHandler);
 
 export default app;
